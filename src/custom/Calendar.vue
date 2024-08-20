95086b83945fc61c03df87823798f4167af871fa
aoyia <2961617189@qq.com>
2024-08-20 12:04:26 +0800

    feat: :sparkles:

--- src/custom/Calendar.vue
+++ src/custom/Calendar.vue
@@ -1,5 +1,8 @@
 <template>
-  <el-calendar v-model="value" />
+  <div class="calendar">
+    <div>自开发日历组件</div>
+    <el-calendar v-model="value" />
+  </div>
 </template>
 
 <script lang="ts" setup>
